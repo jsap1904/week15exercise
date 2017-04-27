7625ae6416a9c9516ee8e21979418baa1e8bec83
jsap1904 <sapinoso.j@gmail.com>
2017-04-26 21:27:46 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('myApp', [])
+var app = angular.module('multiPageApp', ['ngRoute'])
+
+app.config (function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/home.html'
+		})
+		.when('/about', {
+			templateUrl: 'views/about.html'
+		})
+		.otherwise ({
+			redirectTo: '/'
+		})
+})
